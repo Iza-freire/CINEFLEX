818a9466129ff46c3db22e32a5fc217eb4f4850a
IzaFreire <kariny.frei.2000@gmail.com>
2022-12-12 05:17:50 -0300

    Versão 1 - Route funcionando

--- src/App.js
+++ src/App.js
@@ -4,18 +4,22 @@ import HomePage from "./Pages/HomePage";
 import SessionPages from "./Pages/SessionPage";
 import SeatsPage from "./Pages/SeatsPage";
 import SuccessPage from "./Pages/SuccessPage";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <GlobalStyle />
-      <NavBar/>
-      {/**<HomePage/>**/}
-     <SessionPages/>
-      {/*<SuccessPage/>*/}
-    </>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<HomePage />}/>
+        <Route path="/sessoes/:idFilme" element={<SessionPages/>}/>
+        <Route path="/assentos/:idSessao" element={<SeatsPage/>}/>
+        <Route path="/sucesso" element={<SuccessPage/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/Pages/SeatsPage/index.js
+++ src/Pages/SeatsPage/index.js
@@ -2,25 +2,14 @@ import styled from "styled-components"
 import Footer from "../../componentes/Footer/Footer"
 import Forms from "./Forms"
 import Opcoes from "./Opcoes"
+import Seat from "./Seat"
 
 
 export default function SeatsPage(){
     return(
         <HomeContainer>
             Selecione o(s) assento(s)
-            <ListOfContainer>
-                <ButtonContainer>1</ButtonContainer>
-                <ButtonContainer>1</ButtonContainer>
-                <ButtonContainer>1</ButtonContainer>
-                <ButtonContainer>1</ButtonContainer>
-                <ButtonContainer>1</ButtonContainer>
-                <ButtonContainer>1</ButtonContainer>
-                <ButtonContainer>1</ButtonContainer>
-                <ButtonContainer>1</ButtonContainer>
-                <ButtonContainer>1</ButtonContainer>
-                <ButtonContainer>1</ButtonContainer>
-                <ButtonContainer>1</ButtonContainer>
-            </ListOfContainer>
+            <Seat/>
             <Opcoes/>
             <Forms/>
             <Footer />
@@ -37,21 +26,4 @@ const HomeContainer = styled.div`
     font-family: 'Roboto';
     padding-top: 70px;
     margin-top: 40px;
-`
-const ListOfContainer = styled.div`
-    display: flex;
-    flex-wrap: wrap;
-    flex-direction: row;
-    padding: 10px 30px ;
-`
-const ButtonContainer = styled.button`
-        width: 26px;
-        height: 26px;
-        border-radius: 12px;
-        background-color: #C3CFD9;
-        border: 1px solid #808F9D;
-        font-size: 11px;
-        color: black;
-        margin-top: 20px;
-        padding: 5px;
 `
